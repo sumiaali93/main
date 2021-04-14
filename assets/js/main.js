eebf0291dad9ccef951c0563f9169f46541a951d
Sumia <ali.abas2020@yahoo.com>
2021-04-13 21:39:18 -0700

    to push

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(".services-carousel").owlCarousel();
+  items:3;
+  rtl:true;
+nav:true;
+});
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+items:6;
+
+});
+$(document).ready(function(){
+  $(".owl-carousel all").owlCarousel();
+items:6;
+});
+$(document).ready(function(){
+  $(".owl-carousel pic").owlCarousel();
+rtl:true;
+items:4;
+});
+
+
+$(document).ready(function(){
+  $(".hidden").hide();});
+  $(document).ready(function(){
+$(".btn").click(function(){
+$(".hidden").show();
+})});
+$(document).ready(function(){
+  $(".btn").dblclick(function(){
+  $(".hidden").fadeToggle();
+  })});
+
+  
